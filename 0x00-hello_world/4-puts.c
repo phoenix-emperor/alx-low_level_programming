4bc96bc61c3e2de7966a1dd44821a6127e225cdd
Adepoju Fatai <adepojufatai@gmail.com>
2022-12-08 20:19:33 -0800

    Added description to task 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * 
+ * Description: 'This program uses "puts" to print a string of characters.'
+ * 
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-  return (0);
+return (0);
 }
