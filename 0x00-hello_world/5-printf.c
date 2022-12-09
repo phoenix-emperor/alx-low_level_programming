1cd56e0d641ff3d5c08d4e2df4a7c5a265958fb4
Adepoju Fatai <adepojufatai@gmail.com>
2022-12-08 20:28:30 -0800

    Adding script 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Using "printf" to print a string of characters.'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
