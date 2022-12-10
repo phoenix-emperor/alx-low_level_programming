ab54ce1835fabc94b7e06fcf405461569b191904
Adepoju Fatai <adepojufatai@gmail.com>
2022-12-10 15:46:32 -0800

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: 1 (Error)
+ */
+
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "and that piece of art is useful\" - Dor"
+	" a Korpar, 2015-10-19\n");
 
- return (1);
+return (1);
 }
