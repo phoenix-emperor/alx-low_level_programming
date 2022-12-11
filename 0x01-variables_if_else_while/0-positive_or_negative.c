1a86007f5edda7704d9be1541349d530bbb24f78
Adepoju Fatai <adepojufatai@gmail.com>
2022-12-11 06:58:02 -0800

    Pushing task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * This program prints if a random number is either
+ * postive, negative or has a value of zero
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+
+if (n > 0)
+printf("%d is positive.\n", n);
+
+else if (n < 0)
+printf("%d is negative.\n", n);
+
+else if (n == 0)
+printf("%d is zero.\n", n);
+
+return (0);
+}
