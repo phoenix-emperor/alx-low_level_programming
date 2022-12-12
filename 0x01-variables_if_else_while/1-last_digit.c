385a27d722cb1378d3f7a329ec15d6921e48421c
Adepoju Fatai <adepojufatai@gmail.com>
2022-12-12 08:41:46 -0800

    pushing correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int last_digit;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
+/* n = -98; */
 /* your code goes there */
 if (n % 10 > 5)
 {
@@ -29,9 +29,14 @@ last_digit = n % 10;
 printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
 
-else if (n % 10 > 1 && n % 10 < 6)
+else if (n % 10 < 6)
 {
 last_digit = n % 10;
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+}
+
+else if (n % 10 < 0)
+{
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 }
 
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * This program uses putchar to print out letters a-z.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char alpha;
+char new_line;
+new_line = '\n';
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+putchar(alpha);
+}
+
+for (alpha = 'A'; alpha <= 'Z'; alpha++)
+{
+putchar(alpha);
+}
+
+putchar(new_line);
+
+return (0);
+}
