5f2357bf7b0e74226fc0af191ae319bccedce2e8
Adepoju Fatai <adepojufatai@gmail.com>
2022-12-12 19:53:33 -0800

    Pushing task 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all digits from 0 to 9.
+ *
+ * Return - always 0
+ *
+ */
+
+int main (void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+printf("%d", num);
+
+
+printf("\n");
+
+return (0);
+}
