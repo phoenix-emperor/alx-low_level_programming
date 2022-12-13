db4fd3c7d49405f17219a25590c86570c83ec351
Adepoju Fatai <adepojufatai@gmail.com>
2022-12-12 20:13:57 -0800

    pushing task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * This program prints all single digit numbers of base 10
+ * staring from zero to nine, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+
+putchar('n');
+
+return (0);
+}
