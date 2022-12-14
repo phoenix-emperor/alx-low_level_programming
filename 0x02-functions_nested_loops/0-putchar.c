30f65a29abe1d031cd918d679fa61ca701f09196
Adepoju Fatai <adepojufatai@gmail.com>
2022-12-14 01:38:43 -0800

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
 #include "main.h"
-/**
- * main - print _putchar
- * is written by bwave
- * Return: 0
- */
 
+/**
+ * main - Print _putchar
+ * Return: Always 0
+ */
 int main(void)
 {
 _putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
+_putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
